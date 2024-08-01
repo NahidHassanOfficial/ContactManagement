8be86ffbb585153adc2e563496cb669af8e38da4
Nahid Hassan <mdnahidhassan01@gmail.com>
2024-08-01 18:20:09 +0600

    style updated responsivness improved

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -1,16 +1,16 @@
 <x-layouts.home>
-    <div class="card col-md-5 text-center p-3 shadow-sm ">
+    <div class="card col-10 col-md-8 col-lg-5 text-center p-3 shadow-sm ">
         <p class="h3 text-success-emphasis fw-bold mb-3">Create Contact</p>
         <form action="{{ route('store') }}" method="POST" class="container text-start">
-            <div class="row mb-3">
-                <div class="col-6">
+            <div class="row g-3 mb-3">
+                <div class="col-md-6">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
                     @error('name')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control" id="email" name="email"
                         value="{{ old('email') }}">
@@ -20,8 +20,8 @@
                 </div>
 
             </div>
-            <div class="row mb-3">
-                <div class="col-6">
+            <div class="row g-3 mb-3">
+                <div class="col-md-6">
                     <label for="phone" class="form-label">Phone</label>
                     <input type="text" class="form-control" id="phone" name="phone" maxlength="15"
                         value="{{ old('phone') }}">
@@ -29,7 +29,7 @@
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                     <label for="address" class="form-label">Address</label>
                     <input type="text" class="form-control" id="address" name="address"
                         value="{{ old('address') }}">
@@ -39,11 +39,12 @@
                 </div>
 
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-success lg:w-25" id="addBtn">Create Contact</button>
-                </div>
+
+            <div>
+                <button type="submit" class="btn btn-success col-12 col-md-6 col-xl-4" id="addBtn">Create
+                    Contact</button>
             </div>
+
         </form>
     </div>
 </x-layouts.home>
--- resources/views/components/edit.blade.php
+++ resources/views/components/edit.blade.php
@@ -1,16 +1,16 @@
 <x-layouts.home>
-    <div class="card col-md-5 text-center p-3 shadow-sm ">
+    <div class="card col-md-8 col-lg-5 text-center p-3 shadow-sm ">
         <p class="h3 text-success-emphasis fw-bold mb-3">Edit Information</p>
 
         <form action="{{ route('update', $contact->id) }}" method="POST" class="container text-start">
             @method('PUT')
-            <div class="row mb-3">
-                <div class="col-6">
+            <div class="row g-3 mb-3">
+                <div class="col-md-6">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" id="name" name="name"
                         value="{{ $contact->name }}">
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control" id="email" name="email"
                         value="{{ $contact->email }}">
@@ -20,24 +20,25 @@
                 </div>
 
             </div>
-            <div class="row mb-3">
-                <div class="col-6">
+            <div class="row g-3 mb-3">
+                <div class="col-md-6">
                     <label for="phone" class="form-label">Phone</label>
                     <input type="text" class="form-control" id="phone" name="phone" maxlength="15"
                         value="{{ $contact->phone }}">
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                     <label for="address" class="form-label">Address</label>
                     <input type="text" class="form-control" id="address" name="address"
                         value="{{ $contact->address }}">
                 </div>
 
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-warning lg:w-25" id="addBtn">Update Contact</button>
-                </div>
+
+            <div>
+                <button type="submit" class="btn btn-success col-12 col-md-6 col-xl-4" id="addBtn">Update
+                    Contact</button>
             </div>
+
         </form>
     </div>
 </x-layouts.home>
